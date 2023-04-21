57011e2da336854928e8f23ff7e7ab4ef1255b15
rick <pop@pop-os.localdomain>
2023-04-21 16:00:07 +0800

    committed eatwell app

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# eatwell
+# Eatwell app is an where you can recommend good restaurants all over the world
+# This app showcase the use of HTML, CSS, javascript, node.js, ejs.
+# This app showcase the use of backend and storing data into a file and retrieving using ejs.
---
+++ app.js
@@ -0,0 +1,70 @@
+const fs = require('fs');
+const path = require('path');
+
+const express = require('express');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+app.use(express.urlencoded({extended: false}));
+
+app.get('/', function(req, res) {
+  // const htmlFilePath = path.join(__dirname, 'views', 'index.html');
+  // res.sendFile(htmlFilePath);
+  res.render('index');
+});
+
+app.get('/about', function(req, res) {
+  // const htmlFilePath = path.join(__dirname, 'views', 'about.html');
+  // res.sendFile(htmlFilePath);
+  res.render('about');
+});
+
+app.get('/recommend', function(req, res) {
+  // const htmlFilePath = path.join(__dirname, 'views', 'recommend.html');
+  // res.sendFile(htmlFilePath);
+  res.render('recommend');
+});
+
+// important
+app.post('/recommend', function(req, res) {
+  const restaurant = req.body;
+  const filePath = path.join(__dirname, 'data', 'restaurants.json');
+
+  const fileData = fs.readFileSync(filePath);
+  const storedRestaurants = JSON.parse(fileData);
+
+  storedRestaurants.push(restaurant);
+
+  fs.writeFileSync(filePath, JSON.stringify(storedRestaurants));
+
+  res.redirect("/confirm");
+  next();
+});
+
+app.get('/confirm', function(req, res) {
+  // const htmlFilePath = path.join(__dirname, 'views', 'confirm.html');
+  // res.sendFile(htmlFilePath);
+  res.render('confirm');
+});
+
+app.get('/restaurants', function(req, res) {
+  // const htmlFilePath = path.join(__dirname, 'views', 'restaurants.html');
+  // res.sendFile(htmlFilePath);
+  const filePath = path.join(__dirname, 'data', 'restaurants.json');
+
+  const fileData = fs.readFileSync(filePath);
+  const storedRestaurants = JSON.parse(fileData);
+  res.render('restaurants', {NumberOfRestaurants: storedRestaurants.length, restaurants: storedRestaurants});
+});
+
+app.listen(port, hostname, () => {
+  console.log(`Server is running http://${hostname}:${port}/`)
+});
