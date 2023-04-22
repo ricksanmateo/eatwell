9a7316ae3a1eb8e0d322be840de0af2628b86ae5
rick <pop@pop-os.localdomain>
2023-04-22 11:28:27 +0800

    added 404

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const path = require('path');
 
 const express = require('express');
+const uuid = require('uuid');
 
 const hostname = '127.0.0.1';
 const port = 3000;
@@ -36,6 +37,7 @@ app.get('/recommend', function(req, res) {
 // important
 app.post('/recommend', function(req, res) {
   const restaurant = req.body;
+  restaurant.id = uuid.v4();
   const filePath = path.join(__dirname, 'data', 'restaurants.json');
 
   const fileData = fs.readFileSync(filePath);
@@ -63,6 +65,24 @@ app.get('/restaurants', function(req, res) {
   const fileData = fs.readFileSync(filePath);
   const storedRestaurants = JSON.parse(fileData);
   res.render('restaurants', {NumberOfRestaurants: storedRestaurants.length, restaurants: storedRestaurants});
+});
+
+app.get('/restaurants/:id', function(req, res) {
+  const restaurantId = req.params.id;
+
+  const filePath = path.join(__dirname, 'data', 'restaurants.json');
+
+  const fileData = fs.readFileSync(filePath); 
+  const storedRestaurants = JSON.parse(fileData);
+
+  for (const restaurant of storedRestaurants) {
+   if (restaurant.id === restaurantId) {
+    return res.render('restaurants-detail', {restaurant: restaurant})
+   }
+  }
+
+  res.render('404');
+  
 });
 
 app.listen(port, hostname, () => {
